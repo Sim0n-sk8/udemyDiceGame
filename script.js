88dfc2e93b449062c14213c9c43a329584864e6a
Simon <simonswart91@gmail.com>
2025-10-19 15:57:23 +0200

    done pre styling(lazy sunday am i right)

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function spinDice() {
-var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+var randomDiceImage = "dice" + randomNumber1 + ".png";
 
-var randomImageSource = "assets/" + randomDiceImage; //assets/dice1.png - assets/dice6.png
+var randomImageSource = "assets/" + randomDiceImage;
 
 var image1 = document.querySelectorAll("img")[0];
 
@@ -17,5 +17,14 @@ var randomImageSource2 = "assets/dice" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Play 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
 
 }
