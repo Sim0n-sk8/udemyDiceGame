57fe8a2fbb653f3a804a1c3620ca567a9df28c0c
Simon <simonswart91@gmail.com>
2025-10-19 15:14:06 +0200

    mathForD1

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function spinDice() {
     const d1 = document.getElementById("d1");
     const d2 = document.getElementById("d2");
 
-    
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 }
